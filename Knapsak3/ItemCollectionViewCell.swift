0905160f6e5824b01f4fe3e3e85bdf3d034208fc
corrimori <corrine@promptt.com>
2018-09-10 16:47:08 -0700

    add item view and label to collection view

---
+++ Knapsak3/ItemCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCollectionViewCell.swift
+//  Knapsak3
+//
+//  Created by Corrine Morita on 9/10/18.
+//  Copyright © 2018 Corrine Morita. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemLabel: UILabel!
+   
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
+}
--- Knapsak3/ViewController.swift
+++ Knapsak3/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    let items = ["boy_undies", "girl_undies", "jacket", "longsleeve", "shorts", "skirt", "socks", "toothbrush", "tshirt"]
+    
+    let itemImages: [UIImage] = [
+        UIImage(named: "boy_undies")!,
+        UIImage(named: "girl_undies")!,
+        UIImage(named: "jacket")!,
+        UIImage(named: "longsleeve")!,
+        UIImage(named: "shorts")!,
+        UIImage(named: "skirt")!,
+        UIImage(named: "socks")!,
+        UIImage(named: "toothbrush")!,
+        UIImage(named: "tshirt")!
+
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath) as! ItemCollectionViewCell
+        
+        cell.itemLabel.text = items[indexPath.item]
+        cell.itemImageView.image = itemImages[indexPath.item]
+        cell.counterLabel = UILabel()
+        
+        cell.stepper = UIStepper()
+        
+        return cell
+        
+    }
 
 }
 
